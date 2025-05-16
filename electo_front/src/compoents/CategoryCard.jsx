404ffc1429353ae72c0b963e71c9d2d37684c47e
Hanine000 <boukhennoufa.nihan@gmail.com>
2025-05-16 23:51:22 +0100

    update

--- electo_front/src/compoents/CategoryCard.jsx
+++ electo_front/src/compoents/CategoryCard.jsx
@@ -3,10 +3,12 @@ import "../styles/CategoryCard.css";
 
 function CategoryCard({ category, logo }) {
   return (
+    <Link to={`/products/category/${category.toLowerCase().replace(/\s+/g, "-")}`}>
     <div className="category-card">
       <img src={logo} alt={category} />
       <p>{category}</p>
     </div>
+    </Link>
   );
 }
 
